75221313df5713c82b710a587df7b2fbaa1edf9a
huhn <sehe89@gmail.com>
2019-04-04 03:06:31 +0200

    add some order details

--- src/components/OrderDetail.vue
+++ src/components/OrderDetail.vue
@@ -1,16 +1,33 @@
 <template>
   <div v-loading="loading">
     <h1>Order Detail</h1>
-    {{root}}
-    <p>{{order.data.shipping_location}}</p>
-    <p>view on <a :href="'https://www.google.com/maps/search/?api=1&query=' + location.latitude + ',' + location.longitude">google maps</a></p>
-    <pre >{{order}}</pre>
+
+    <el-form ref="form" label-position="top">
+      <el-form-item label="ID">
+        <el-input v-model="order.data.id" readonly></el-input>
+      </el-form-item>
+      <el-form-item label="Status">
+        <el-input v-model="order.status" readonly></el-input>
+      </el-form-item>
+      <el-form-item label="Shipping location">
+        <el-input v-model="order.data.shipping_location" readonly></el-input>
+        <p>
+          view on
+          <a
+            :href="'https://www.google.com/maps/search/?api=1&query=' + location.latitude + ',' + location.longitude"
+          >google maps</a>
+        </p>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">Commit pick up</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
 import { fetch } from "@/utils/MAM";
-const iotaAreaCodes = require('@iota/area-codes');
+const iotaAreaCodes = require("@iota/area-codes");
 
 export default {
   props: ["root"],
@@ -32,31 +49,29 @@ export default {
   methods: {
     loadData: async function() {
       this.loading = true;
-      console.log("load data from root: ", this.root);
       try {
-          await fetch(
-            this.root,
-            "restriced",
-            "TEST",
-            this.appendToMessages,
-            this.fetchComplete
-          );
-        } catch (err) {
-          console.log("error", err)
-        }
+        await fetch(
+          this.root,
+          "restriced",
+          "TEST",
+          this.appendToMessages,
+          this.fetchComplete
+        );
+      } catch (err) {
+        // handle error
+      }
       this.loading = false;
     },
     appendToMessages(message) {
-      console.log("message", message);
       this.messages.push(message);
     },
-    fetchComplete() { 
+    fetchComplete() {
       this.order = this.sortedMessages[0];
-      let iac = this.order.data.shipping_location
-      console.log("iac", iac);
-      console.log("vaild?", iotaAreaCodes.isValid(iac));
-      this.location = iotaAreaCodes.decode(iac)
-      console.log("this.location", this.location)
+      let iac = this.order.data.shipping_location;
+      this.location = iotaAreaCodes.decode(iac);
+    },
+    onSubmit() {
+      console.log("submit!");
     }
   },
   computed: {
